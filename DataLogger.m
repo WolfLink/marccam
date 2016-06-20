068f14141aeedc2be76413a09376908098ef5c9b
Marc Davis <marc.davis@berkeley.edu>
2016-06-20 15:11:39 -0700

    Minor improvements

--- DataLogger.m
+++ DataLogger.m
@@ -13,8 +13,12 @@ classdef DataLogger < handle
     methods
         function obj = DataLogger(id)
             obj.stringid = id;
-            obj.fileid = fopen(sprintf('Log %s %s.txt', id, char(datetime('now', 'Format', 'yyyy-MM-dd'))),'a+');
-            fprintf(obj.fileid, 'X,Y,TIME');
+            fname = sprintf('Log %s %s.txt', id, char(datetime('now', 'Format', 'yyyy-MM-dd')));
+            e = exist(fname, 'file');
+            obj.fileid = fopen(fname,'a+');
+            if ~e
+                fprintf(obj.fileid, 'X,Y,TIME');
+            end
         end
         function writeTrackEntry(obj)
             fprintf(obj.fileid, '\n%f,%f,%s', obj.storedx, obj.storedy, char(datetime('now', 'Format', 'HH:mm:ss')));
